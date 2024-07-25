797bdb870216ce9f799c92cd29c4fae8f1781622
Frederico Dominici <contato@freddominici.com>
2024-07-25 18:42:15 -0300

    window location game.html

--- js/login.js
+++ js/login.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const loginInput = document.querySelector('.loginInput');
     const loginBtn = document.querySelector('.loginBtn');
+    const loginForm = document.querySelector('.loginForm')
 
-    const validadeInput = ({ target })=>{
+    const validateInput = ({ target })=>{
         if (target.value.length > 2){
             loginBtn.removeAttribute('disabled');
         } else {
-            loginBtn.setAttribute('disabled', '')
+            loginBtn.setAttribute('disabled', '');
         }
     }
 
-    loginInput.addEventListener('input', validadeInput);
+    loginInput.addEventListener('input', validateInput);
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        localStorage.setItem('player', loginInput.value);
+        window.location = 'game.html'
+    }
+
+    loginForm.addEventListener('submit', handleSubmit)
 })
